561df4a7867afdd9f6214aa5d63d70cce04b6315
Maddi Chilton <maddichilton@gmail.com>
2019-05-02 20:05:48 -0500

    aaa

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,10 +7,48 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:LaunchCode@localh
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
+app.secret_key = 'y337kGcys&zP3B'
 
-# RECENT: functional until new post, upon which presents TypeError: __init__() missing 1 required positional argument: 'author'
-# TO DO: in Blogz assignment: add Login and Signup pages
-# ELSE: add author field to new post
+# TO DO: in Blogz assignment: add dynamic user pages
+# ELSE: errors in login/signup???
+
+def validity(e, p, pv):
+    p_error_msg = ""
+    pv_error_msg = ""
+    e_error_msg = ""
+
+    if " " in p or len(p) < 3 or len(p) > 20:
+        p_error_msg = "Invalid password"
+    if p != pv:
+        pv_error_msg += "Passwords must match"
+    if e != "":
+        ats = 0
+        dots = 0
+        for i in e:
+            if i == "@":
+                ats += 1
+            if i == ".":
+                dots += 1
+            else:
+                continue
+        if ats != 1 or dots != 1:
+            e_error_msg = "Invalid email"
+        if " " in e or len(e) < 3 or len(e) > 20:
+            e_error_msg = "Invalid email"
+    if p == "":
+        p_error_msg = "Field cannot be blank"
+    if pv == "":
+        pv_error_msg = "Field cannot be blank"
+    if e == "":
+        e_error_msg = "Field cannot be blank"
+    
+    if p_error_msg == "" and pv_error_msg == "" and e_error_msg == "":
+        allvalid = True
+    else:
+        allvalid = False
+
+    return e_error_msg, p_error_msg, pv_error_msg, allvalid
+
 
 
 class Blog(db.Model):
@@ -28,36 +66,30 @@ class Blog(db.Model):
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
+    email = db.Column(db.String(50))
     password = db.Column(db.String(50))
     blogs = db.relationship('Blog', backref='author')
 
-    def __init__(self, username, password):
-        self.username = username
+    def __init__(self, email, password):
+        self.email = email
         self.password = password
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
-    #if request.method == 'POST':
-    #    blog_body = request.form['body']
-    #    blog_title = request.form['title']
-    #    new_blog = Blog(blog_title, blog_body)
-    #    db.session.add(new_blog)
-    #    db.session.commit()
-
     blogs = Blog.query.all()
     blogs_length = int(len(blogs))
     recent_id = blogs_length - 1
-    if not recent_id <= 0:
+    if not recent_id < 0:
         recent_blog = blogs[recent_id]
     else:
         recent_blog = ""
     
+    users = User.query.all()
     
     return render_template('index.html',title="build a blog!", 
-        blogs=blogs, recent_blog=recent_blog)
+        blogs=blogs, recent_blog=recent_blog, users=users)
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
@@ -66,6 +98,8 @@ def new_post():
     blog_body = ""
     blog_title = ""
     error = ""
+
+    current_user = User.query.filter_by(email=session["user"]).first()
 
     if request.method == 'POST':
         blog_body = request.form['body']
@@ -83,7 +117,7 @@ def new_post():
                 title_error = ""
             return render_template('newpost.html',title="build a blog!", blogs=blogs, 
         body_error=body_error, title_error=title_error, old_title=blog_title, old_body=blog_body)
-        new_blog = Blog(blog_title, blog_body)
+        new_blog = Blog(blog_title, blog_body, current_user)
         db.session.add(new_blog)
         db.session.commit()
 
@@ -111,6 +145,67 @@ def blog_posts():
 
     return render_template('blog.html',title="build a blog!", 
         blogs=blogs)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user.password == password:
+            session['user'] = user.email
+            flash("Logged in")
+            return redirect('/')
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        signup_attempt = validity(email, password, verify)
+
+        e_error = signup_attempt[0]
+        pw_error = signup_attempt[1]
+        pv_error = signup_attempt[2]
+
+        if signup_attempt[3] == False:
+            if e_error != "":
+                flash(e_error)
+            if pw_error != "":
+                flash(pw_error)
+            if pv_error != "":
+                flash(pv_error)
+            return redirect ("/signup")
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['user'] = new_user.email
+            return redirect('/')
+        else:
+            flash('User already exists', 'error')
+
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    del session['user']
+    return redirect('/')
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup', 'index', 'blog_posts']
+    if request.endpoint not in allowed_routes and 'user' not in session:
+        return redirect('/login')
 
 
 if __name__ == '__main__':
